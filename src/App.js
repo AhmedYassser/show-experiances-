e2dc43a05c8f1b7d015191a46fbe9439b14b90c8
AHMED YASSER <mitso2710@gmail.com>
2022-04-19 23:08:16 +0200

    First one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { GiEntangledTyphoon } from 'react-icons/gi'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [people, setPeople] = useState([]);
+  const [index, setindex] = useState(0);
+  const [loading, setLoading] = useState(true)
+
+  async function getinfo() {
+    let { data } = await axios.get('https://course-api.com/react-tabs-project')
+    setPeople(data)
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getinfo()
+  }, [])
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
+  const { company, dates, duties, title } = people[index];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {people.map((item, indexjob) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setindex(indexjob)}
+                className={`job-btn ${indexjob === index && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <GiEntangledTyphoon className="job-icon"></GiEntangledTyphoon>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <button type="button" className="btn">
+        more info
+      </button>
+    </section>
   );
 }
 
